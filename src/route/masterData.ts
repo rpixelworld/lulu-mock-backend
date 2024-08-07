6fdb5a757c72b3dd824f0d8fec7357695cc899a2
李杏 <lixing@lixingdeMacBook-Pro.local>
2024-08-06 17:37:40 -0700

    create

--- src/route/masterData.ts
+++ src/route/masterData.ts
@@ -2,6 +2,6 @@ import {Router} from "express";
 import MasterDataController from "../controller/MasterDataController";
 
 const masterDataRouter = Router();
-masterDataRouter.post("/tax-rates/:province", MasterDataController.getTaxRate);
+masterDataRouter.get("/tax-rates/:province", MasterDataController.getTaxRate);
 
 export default masterDataRouter;
