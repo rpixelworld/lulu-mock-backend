07a999c1341edbc44461dac24e8f4e9dc9b386f2
Heng Zhang <hengzhang@MacBook-Air-M3.local>
2024-08-09 16:03:22 -0400

    pay/cancel/ship-status

--- src/controller/OrderController.ts
+++ src/controller/OrderController.ts
@@ -1,13 +1,11 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import gDB from '../InitDataSource';
 import ResponseHelper from './ResponseHelper';
-import { logger } from '../LoggerHelper';
-import { ErrorCode } from '../common/ErrorCode';
-import { FindManyOptions, FindOneOptions, Repository, SelectQueryBuilder } from 'typeorm';
-import { Order } from '../entity/Order.entity';
-import { OrderStatus } from '../common/OrderStatus';
-import user from '../route/user';
-import { create } from 'node:domain';
+import {logger} from '../LoggerHelper';
+import {ErrorCode} from '../common/ErrorCode';
+import {Repository, SelectQueryBuilder} from 'typeorm';
+import {Order} from '../entity/Order.entity';
+import {OrderStatus} from '../common/OrderStatus';
 
 class OrderController {
 	static async getUserOrders(req: Request, resp: Response) {
@@ -96,11 +94,90 @@ class OrderController {
 
 	static async placeOrder(req: Request, resp: Response) {}
 
-	static async payOrder(req: Request, resp: Response) {}
+	static async payOrder(req: Request, resp: Response) {
+		const { orderId } = req.params;
+		if (!Number.isInteger(Number(orderId))) {
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Invalid order id'));
+		}
+		try {
+			const repo = gDB.getRepository(Order);
+			const order = await repo.findOneOrFail({where:{id: Number(orderId)}});
+			if (!order) {
+				return resp
+					.status(404)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND,'order not found'))
+			}
+			order.status = OrderStatus.PAID
+			await repo.save(order);
+			return resp
+				.status(200)
+				.send(ResponseHelper.generateSuccessResult(order));
+		} catch (e) {
+			logger.error('Exception while paying for order', e)
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, e.driverError));
+		}
 
-	static async cancelOrder(req: Request, resp: Response) {}
+	}
 
-	static async shipOrder(req: Request, resp: Response) {}
+	static async cancelOrder(req: Request, resp: Response) {
+		const { orderId } = req.params;
+		if (!Number.isInteger(Number(orderId))) {
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Invalid order id'));
+		}
+		try {
+			const repo = gDB.getRepository(Order);
+			const order = await repo.findOneOrFail({where:{id: Number(orderId)}});
+			if (!order) {
+				return resp
+					.status(404)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND,'order not found'))
+			}
+			order.status = OrderStatus.CANCELLED
+			await repo.save(order);
+			return resp
+				.status(200)
+				.send(ResponseHelper.generateSuccessResult(order));
+		} catch (e) {
+			logger.error('Exception while paying for order', e)
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, e.driverError));
+		}
+	}
+
+	static async shipOrder(req: Request, resp: Response) {
+		const { orderId } = req.params;
+		if (!Number.isInteger(Number(orderId))) {
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Invalid order id'));
+		}
+		try {
+			const repo = gDB.getRepository(Order);
+			const order = await repo.findOneOrFail({where:{id: Number(orderId)}});
+			if (!order) {
+				return resp
+					.status(404)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND,'order not found'))
+			}
+			order.status = OrderStatus.SHIPPED
+			await repo.save(order);
+			return resp
+				.status(200)
+				.send(ResponseHelper.generateSuccessResult(order));
+		} catch (e) {
+			logger.error('Exception while paying for order', e)
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, e.driverError));
+		}
+	}
 }
 
 export default OrderController;
