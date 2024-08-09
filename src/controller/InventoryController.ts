e7207d79c1722c63b3bbb7d1ec94ae7a8e65d73e
Rita Zhou <rzhou150@gmail.copm>
2024-08-09 13:02:30 -0700

    update for merge

--- src/controller/InventoryController.ts
+++ src/controller/InventoryController.ts
@@ -1,110 +1,141 @@
-import {Request, Response} from "express";
-import gDB from "../InitDataSource";
-import ResponseHelper from "./ResponseHelper";
-import {logger} from "../LoggerHelper";
-import {Inventory} from "../entity/Inventory.entity";
-import {ErrorCode} from "../common/ErrorCode";
-import {validate} from "class-validator";
+import { Request, Response } from 'express';
+import gDB from '../InitDataSource';
+import ResponseHelper from './ResponseHelper';
+import { logger } from '../LoggerHelper';
+import { Inventory } from '../entity/Inventory.entity';
+import { ErrorCode } from '../common/ErrorCode';
+import { validate } from 'class-validator';
 
 class InventoryController {
+	public static get repo() {
+		return gDB.getRepository(Inventory);
+	}
+	// find all products with productId
+	static async getProductInventory(req: Request, resp: Response) {
+		try {
+			const products = await InventoryController.repo.find({ where: { productId: req.params.productId } });
+			if (!products || products.length === 0) {
+				logger.error('product is not found');
+				return resp
+					.status(400)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'));
+			}
+			return resp.status(200).json(ResponseHelper.generateSuccessResult(products));
+		} catch (error) {
+			logger.error('error getting product', error);
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'));
+		}
+	}
 
-  public static get repo(){
-    return gDB.getRepository(Inventory);
-  }
-  // find all products with productId
-  static async getProductInventory(req: Request, resp: Response) {
+	// find product and its color and size
+	static async getInventory(req: Request, resp: Response) {
+		const { productId, colorId, size } = req.params;
+		try {
+			const product = await InventoryController.repo.find({
+				where: {
+					productId: productId,
+					colorId: colorId,
+					size: size,
+				},
+			});
+			// verify params
+			if (!product || product.length === 0 || !colorId || colorId.length === 0 || !size || size.length === 0) {
+				logger.error('product is not found');
+				return resp
+					.status(400)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'));
+			}
 
-    try {
-      const products =await InventoryController.repo.find({where: {productId: req.params.productId}});
-      if (!products || products.length===0) {
-        logger.error('product is not found');
-        return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'))
-      } return resp.status(200).json(ResponseHelper.generateSuccessResult(products))
-    } catch (error) {
-      logger.error('error getting product', error);
-      return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'))
-    }
-  }
-  // find product and its color and size
-  static async getInventory(req: Request, resp: Response) {
-    const {productId,colorId,size} = req.params;
-    try {
-      const product =await InventoryController.repo.find({where: {
-        productId: productId,
-        colorId: colorId,
-        size: size,}});
-      // verify params
-      if (!product || product.length===0 || !colorId || colorId.length===0 || !size || size.length===0) {
-        logger.error('product is not found');
-        return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'))
-      }
+			return resp.status(200).json(ResponseHelper.generateSuccessResult(product));
+		} catch (error) {
+			logger.error('error getting product', error);
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'));
+		}
+	}
 
-      return resp.status(200).json(ResponseHelper.generateSuccessResult(product))
-    } catch (error) {
-      logger.error('error getting product', error);
-      return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'))
-    }
-  }
+	static async updateInventory(req: Request, resp: Response) {
+		const { productId, colorId, size } = req.params;
+		const { stock } = req.body;
+		// verify params
+		if (!productId || !colorId || !size || stock === undefined) {
+			logger.error('Invalid request parameters');
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.INVALID_REQUEST, 'Invalid request parameters'));
+		}
+		// verify type of params
+		if (typeof stock !== 'number') {
+			logger.error('Stock is not a number');
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Stock must be a number'));
+		}
 
-  static async getInventoryOfProductAndColor(req: Request, resp: Response) {
-    const {productId,colorId} = req.params;
-    try {
-      const product =await InventoryController.repo.find({where: {
-          productId: productId,
-          colorId: colorId,}});
-      // verify params
-      if (!product || product.length===0 || !colorId || colorId.length===0 ) {
-        logger.error('product is not found');
-        return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'))
-      }
+		try {
+			// begin looking and comparing
+			const product = await InventoryController.repo.findOne({
+				where: {
+					productId: productId,
+					colorId: colorId,
+					size: size,
+				},
+			});
+			if (!product) {
+				logger.error('product is not found');
+				return resp
+					.status(400)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'));
+			}
+			// update data
+			product.stock = stock;
+			// verify type of data after update
+			const err = await validate(product);
+			if (err.length > 0) {
+				logger.error('stock is not number');
+				return resp
+					.status(400)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'stock is not number'));
+			}
+			// save to the entity
+			await InventoryController.repo.save(product);
+			return resp.status(200).send(ResponseHelper.generateSuccessResult(product));
+		} catch (error) {
+			logger.error('error getting product', error);
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'));
+		}
+	}
 
-      return resp.status(200).json(ResponseHelper.generateSuccessResult(product))
-    } catch (error) {
-      logger.error('error getting product', error);
-      return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'))
-    }
-  }
+	static async getInventoryOfProductAndColor(req: Request, resp: Response) {
+		const { productId, colorId } = req.params;
+		try {
+			const product = await InventoryController.repo.find({
+				where: {
+					productId: productId,
+					colorId: colorId,
+				},
+			});
+			// verify params
+			if (!product || product.length === 0 || !colorId || colorId.length === 0) {
+				logger.error('product is not found');
+				return resp
+					.status(400)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'));
+			}
 
-  static async updateInventory(req: Request, resp: Response) {
-    const {productId,colorId,size} = req.params;
-    const {stock}= req.body;
-    // verify params
-    if (!productId || !colorId || !size || stock===undefined) {
-      logger.error('Invalid request parameters');
-      return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.INVALID_REQUEST, 'Invalid request parameters'));
-    }
-    // verify type of params
-    if (typeof stock !== 'number') {
-      logger.error('Stock is not a number');
-      return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Stock must be a number'));
-    }
-
-    try {
-      // begin looking and comparing
-      const product =await InventoryController.repo.findOne({where: {
-          productId: productId,
-          colorId: colorId,
-          size: size,}});
-      if (!product) {
-        logger.error('product is not found');
-        return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.PRODUCT_NOT_FOUND, 'product is not found'))
-      }
-      // update data
-      product.stock= stock
-      // verify type of data after update
-      const err= await validate(product)
-      if (err.length>0) {
-        logger.error('stock is not number')
-        return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'stock is not number'))
-      }
-      // save to the entity
-      await InventoryController.repo.save(product);
-      return resp.status(200).send(ResponseHelper.generateSuccessResult(product))
-    } catch (error) {
-      logger.error('error getting product', error);
-      return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'))
-    }
-  }
+			return resp.status(200).json(ResponseHelper.generateSuccessResult(product));
+		} catch (error) {
+			logger.error('error getting product', error);
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'));
+		}
+	}
 }
 
 export default InventoryController;
--- src/controller/OrderController.ts
+++ src/controller/OrderController.ts
@@ -1,60 +1,209 @@
-import {Request, Response} from "express";
-import gDB from "../InitDataSource";
-import {Order} from "../entity/Order.entity";
-import {logger} from "../LoggerHelper";
-import ResponseHelper from "./ResponseHelper";
-import {ErrorCode} from "../common/ErrorCode";
-import {validate} from "class-validator";
-import {CheckIdController} from "./CheckIdController";
-import {ValidateOrder} from "../common/ValidateOrder";
+import { Request, Response } from 'express';
+import gDB from '../InitDataSource';
+import { Order } from '../entity/Order.entity';
+import { logger } from '../LoggerHelper';
+import ResponseHelper from './ResponseHelper';
+import { ErrorCode } from '../common/ErrorCode';
+import { validate } from 'class-validator';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { User } from '../entity/User.entity';
+import { ShippingAddress } from '../entity/ShippingAddress.entity';
+import { OrderStatus } from '../common/OrderStatus';
 
-class OrderController extends CheckIdController{
+class OrderController {
+	static async getUserOrders(req: Request, resp: Response) {
+		const validationMsg = validateQueryOrder(req);
+		if (validationMsg) {
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, validationMsg));
+		}
 
-  public static get repo(){
-    return gDB.getRepository(Order);
-  }
-  static async getUserOrders(req: Request, resp: Response) {}
+		const repo = gDB.getRepository(Order);
+		try {
+			const { pageNo = '1', pageSize = '5' } = req.query;
+			const queryBuilder: SelectQueryBuilder<Order> = createQueryBuilder(repo, req);
+			const [orders, total] = await queryBuilder.getManyAndCount();
 
-  static async getAllOrders(req: Request, resp: Response) {}
+			return resp.status(200).send(
+				ResponseHelper.generateSuccessResult({
+					pagination: ResponseHelper.generatePaginationParams(
+						Number(pageNo),
+						Number(pageSize),
+						total,
+						orders.length
+					),
+					orders: orders,
+				})
+			);
+		} catch (e) {
+			logger.error('Exception find orders', e);
+			return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, e.driverError));
+		}
+	}
 
-  static async download(req: Request, resp: Response) {}
+	static async getAllOrders(req: Request, resp: Response) {
+		const validationMsg = validateQueryOrder(req);
+		if (validationMsg) {
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, validationMsg));
+		}
 
-  static async placeOrder(req: Request, resp: Response) {
+		const repo = gDB.getRepository(Order);
+		try {
+			const { pageNo = '1', pageSize = '5' } = req.query;
+			const queryBuilder: SelectQueryBuilder<Order> = createQueryBuilder(repo, req);
+			const [orders, total] = await queryBuilder.getManyAndCount();
 
-    const order = Object.assign(new Order(), req.body);
-    // const {n}
-    try {
-      const errors = await validate(order)
-      if (errors.length>0) {
-        return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR,errors))
-      }
-      console.log('userId',req.body.userId);
-      const [userIdRes,shippingAddressIdRes]= await ValidateOrder(req.body.userId,req.body.shippingAddressId)
-      // console.log('o2')
-      // check res
-      if (!userIdRes.entities?.length || !shippingAddressIdRes.entities?.length) {
-        return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Invalid userId, shipping address, or order items'));
-      }
-      order.user= userIdRes.entities[0]
-      order.shippingAddress= shippingAddressIdRes.entities[0]
+			return resp.status(200).send(
+				ResponseHelper.generateSuccessResult({
+					pagination: ResponseHelper.generatePaginationParams(
+						Number(pageNo),
+						Number(pageSize),
+						total,
+						orders.length
+					),
+					orders: orders,
+				})
+			);
+		} catch (e) {
+			logger.error('Exception find orders', e);
+			return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, e.driverError));
+		}
+	}
 
+	static async placeOrder(req: Request, resp: Response) {
+		logger.info('placing order');
+		try {
+			const { userId, shippingAddressId } = req.body;
 
-      await OrderController.repo.save(order)
+			if (userId && !Number.isInteger(Number(userId))) {
+				return resp
+					.status(400)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Invalid user id'));
+			}
+			if (shippingAddressId && !Number.isInteger(Number(shippingAddressId))) {
+				return resp
+					.status(400)
+					.send(
+						ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Invalid shippingAddress id')
+					);
+			}
 
-      return resp.status(200).send(ResponseHelper.generateSuccessResult(order))
+			const user: User = await gDB.getRepository(User).findOne({ where: { id: userId } });
+			if (!user) {
+				return resp
+					.status(400)
+					.send(ResponseHelper.generateFailureResult(ErrorCode.USER_NOT_EXIST, 'User not exist'));
+			}
+			const shippingAddress = await gDB
+				.getRepository(ShippingAddress)
+				.findOne({ where: { id: shippingAddressId } });
+			if (!shippingAddress) {
+				return resp
+					.status(400)
+					.send(
+						ResponseHelper.generateFailureResult(ErrorCode.ADDRESS_NOT_EXIST, 'Shipping address not exist')
+					);
+			}
 
-    } catch (error) {
-      logger.error('error place order', error);
-      return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'))
-    }
+			let order: Order = Object.assign(new Order(), req.body);
+			const errors = await validate(order);
+			if (errors.length > 0) {
+				return resp.status(400).send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, errors));
+			}
+			order.user = user;
+			order.shippingAddress = shippingAddress;
+			order.status = OrderStatus.CREATED;
 
-  }
+			await gDB.getRepository(Order).save(order);
 
-  static async payOrder(req: Request, resp: Response) {}
+			return resp.status(200).send(ResponseHelper.generateSuccessResult(order));
+		} catch (error) {
+			logger.error('error place order', error);
+			return resp
+				.status(500)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, 'internal server error'));
+		}
+	}
 
-  static async cancelOrder(req: Request, resp: Response) {}
+	static async getOneOrder(req: Request, resp: Response) {
+		const { orderId } = req.params;
+		if (!Number.isInteger(Number(orderId))) {
+			return resp
+				.status(400)
+				.send(ResponseHelper.generateFailureResult(ErrorCode.VALIDATION_ERROR, 'Invalid order id'));
+		}
 
-  static async shipOrder(req: Request, resp: Response) {}
+		try {
+			const order: Order = await gDB.getRepository(Order).findOne({
+				where: { id: Number(orderId) },
+				relations: ['orderItems'],
+			});
+			return resp.status(200).send(ResponseHelper.generateSuccessResult(order));
+		} catch (e) {
+			logger.error('Exception find orders', e);
+			return resp.status(500).send(ResponseHelper.generateFailureResult(ErrorCode.DB_ERROR, e.driverError));
+		}
+	}
+
+	static async download(req: Request, resp: Response) {}
+
+	static async payOrder(req: Request, resp: Response) {}
+
+	static async cancelOrder(req: Request, resp: Response) {}
+
+	static async shipOrder(req: Request, resp: Response) {}
 }
 
 export default OrderController;
+
+function validateQueryOrder(req: Request): string {
+	const { userId } = req.params;
+	logger.info('find orders by userId=', userId, Number.isInteger(userId));
+	if (userId && !Number.isInteger(Number(userId))) {
+		return 'Invalid user id';
+	}
+
+	const { email, orderNumber, orderStatus } = req.body;
+	if (orderNumber && !Number.isInteger(Number(orderNumber))) {
+		return 'Invalid order number';
+	}
+
+	if (orderStatus && (!Number.isInteger(Number(orderStatus)) || !Object.values(OrderStatus).includes(orderStatus))) {
+		return 'Invalid order status';
+	}
+
+	const { pageNo = '1', pageSize = '5' } = req.query;
+	if (!Number.isInteger(Number(pageNo)) || !Number.isInteger(Number(pageSize))) {
+		return 'Invalid page number or page size';
+	}
+}
+
+function createQueryBuilder(repo: Repository<Order>, req: Request): SelectQueryBuilder<Order> {
+	let queryBuilder: SelectQueryBuilder<Order> = repo.createQueryBuilder('order');
+
+	const { userId } = req.params;
+	const { email, orderNumber, orderStatus } = req.body;
+	if (userId || email) {
+		queryBuilder.innerJoin('order.user', 'user');
+		if (userId) {
+			queryBuilder.where('user.id=:userId', { userId: Number(userId) });
+		} else {
+			queryBuilder.where('user.email=:email', { email: email });
+		}
+	}
+	if (orderNumber) {
+		queryBuilder.where('id=:orderId', { orderId: Number(orderNumber) });
+	}
+	if (orderStatus) {
+		queryBuilder.where('status=:orderStatus', { orderStatus: Number(orderStatus) });
+	}
+
+	const { pageNo = '1', pageSize = '5' } = req.query;
+	queryBuilder.skip((Number(pageNo) - 1) * Number(pageSize));
+	queryBuilder.take(Number(pageSize));
+
+	return queryBuilder;
+}
