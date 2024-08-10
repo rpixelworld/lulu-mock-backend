58c329efe200ea163a4e83806c1e874809337928
李杏 <lixing@lixingdeMacBook-Pro.local>
2024-08-10 16:45:29 -0700

    receipt

--- src/route/order.ts
+++ src/route/order.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express';
-import UserController from '../controller/UserController';
 import OrderController from '../controller/OrderController';
 import { validateAdminJwt, validateJwt } from '../middleware/JwtValidator';
+import {ReceiptController} from "../controller/ReceiptController";
 
 const orderRouter = Router();
+
 orderRouter.post('/:userId', [validateJwt], OrderController.getUserOrders);
 orderRouter.post('/', [validateAdminJwt], OrderController.getAllOrders);
-orderRouter.get('/:orderId', [validateJwt], OrderController.getOneOrder);
+orderRouter.get('/:orderId',[validateJwt], OrderController.getOneOrder);
 orderRouter.get('/:orderId/download/:type', [validateJwt], OrderController.download);
 orderRouter.post('/', [validateJwt], OrderController.placeOrder);
 orderRouter.post('/:orderId/pay', [validateJwt], OrderController.payOrder);
 orderRouter.post('/:orderId/cancel', [validateJwt], OrderController.cancelOrder);
 orderRouter.post('/:orderId/ship', [validateAdminJwt], OrderController.shipOrder);
 
+
+
+
+orderRouter.get('/:orderId/receipt',[validateJwt], ReceiptController.getReceiptPDF);
 export default orderRouter;
