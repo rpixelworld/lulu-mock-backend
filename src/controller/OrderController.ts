7612cb4313f68ab661e97a1460f2381225ab43d1
Rita Zhou <rzhou150@gmail.copm>
2024-08-21 14:47:58 -0700

    fix order

--- src/controller/OrderController.ts
+++ src/controller/OrderController.ts
@@ -291,7 +291,7 @@ function createQueryBuilder(user: User, repo: Repository<Order>, req: Request): 
 		queryBuilder.innerJoin('order.user', 'user');
 		queryBuilder.where('user.id=:userId', { userId: Number(user.id) });
 	} else {
-		if (email && email.trim()!='') {
+		if (email && email.trim() != '') {
 			queryBuilder.innerJoin('order.user', 'user');
 			queryBuilder.where('user.email=:email', { email: email });
 		}
